162bad9994308f6b332e9eac1e98a51446895223
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-11-20 18:21:21 -0300

    chore: configure env

--- start/env.ts
+++ start/env.ts
@@ -28,4 +28,13 @@ export default await Env.create(new URL('../', import.meta.url), {
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string(),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for Telegram API
+  |----------------------------------------------------------
+  */
+  API_ID: Env.schema.string(),
+  API_HASH: Env.schema.string(),
+  STRING_SESSION: Env.schema.string(),
 })
