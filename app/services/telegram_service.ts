84ee256ad668be8294a9a1446c39616c842f8e53
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-11-30 14:26:28 -0300

    refactor: implement telegram repository

--- app/services/telegram_service.ts
+++ app/services/telegram_service.ts
@@ -9,24 +9,31 @@ import TelegramRepository from '#repositories/telegram_repository'
 export class TelegramService {
   constructor(private telegramRepository: TelegramRepository) {}
 
-  async getVideoInfo() {
+  async getVideoInfo(): Promise<Api.Document> {
     const channelId = bigInt('-1001774402469')
     const channel = await this.telegramRepository.client().getEntity(channelId)
     const messages = await this.telegramRepository.client().getMessages(channel, { limit: 2 })
+
+    if (!messages.length) {
+      throw new Error('No messages found in the provided channel')
+    }
 
     const post = messages[0] as Api.Message & {
       media: Api.MessageMediaDocument & { document: Api.Document }
     }
 
-    if (!post.media || !post.media.document) throw new Error('No media found')
+    if (!post?.media?.document) {
+      throw new Error('No media found in the provided message')
+    }
 
-    const document = post.media.document
-
-    return { size: document.size, document }
+    return post.media.document
   }
 
-  async getVideoStream(start: number, end: number) {
-    const { document } = await this.getVideoInfo()
+  async getVideoStream(start: number, end: number): Promise<{ stream: Readable }> {
+    const document = await this.getVideoInfo()
+
+    const fileSize = document.size
+    const downloadParams = this.computeDownloadParams(fileSize)
 
     const iterable = this.telegramRepository.client().iterDownload({
       file: new Api.InputDocumentFileLocation({
@@ -37,15 +44,45 @@ export class TelegramService {
       }),
       offset: bigInt(start),
       limit: end - start + 1,
-      requestSize: 1024 * 1024 * 2, // 2MB
-      chunkSize: 1024 * 1024 * 2, // 2MB
-      stride: 1024 * 1024 * 2, // 2MB
+      requestSize: downloadParams.requestSize,
+      chunkSize: downloadParams.chunkSize,
+      stride: downloadParams.stride,
       dcId: document.dcId,
       fileSize: document.size,
     })
 
     const stream = Readable.from(iterable)
-
     return { stream }
+  }
+
+  private computeDownloadParams(fileSize: bigInt.BigInteger) {
+    const sizeInMB = Number(fileSize) / (1024 * 1024) // Convert to MB
+
+    let params = {
+      chunkSize: 4 * 1024 * 1024,
+      requestSize: 4 * 1024 * 1024,
+      stride: 4 * 1024 * 1024,
+    }
+
+    if (sizeInMB <= 10) {
+      params = {
+        chunkSize: 512 * 1024,
+        requestSize: 512 * 1024,
+        stride: 512 * 1024,
+      }
+    } else if (sizeInMB <= 100) {
+      params = {
+        chunkSize: 1024 * 1024,
+        requestSize: 1024 * 1024,
+        stride: 1024 * 1024,
+      }
+    }
+
+    const maxAllowedSize = 512 * 1024 * 1024 // 512 MB
+    return {
+      chunkSize: Math.min(params.chunkSize, maxAllowedSize),
+      requestSize: Math.min(params.requestSize, maxAllowedSize),
+      stride: Math.min(params.stride, maxAllowedSize),
+    }
   }
 }
