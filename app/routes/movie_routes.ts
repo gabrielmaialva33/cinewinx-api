e712085e46bf298e66e4d87011738b8d279c1674
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-12-01 21:22:46 -0300

    fix: build project

--- app/routes/movie_routes.ts
+++ app/routes/movie_routes.ts
@@ -5,7 +5,6 @@ const MoviesController = () => import('#controllers/movies_controller')
 router
   .group(() => {
     router.get('/', [MoviesController, 'paginate']).as('movies.paginate')
-    router.get('/stream', [StreamsController, 'stream']).as('movies.stream')
     router.get('/videos', [StreamsController, 'video']).as('movies.video')
     router.get('/images', [MoviesController, 'images']).as('movies.images')
     router.get('/:id', [MoviesController, 'get']).as('movies.get')
